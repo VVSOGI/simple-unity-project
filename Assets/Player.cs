10f3501b366733e3ed580ee1d539e11cd2beb12c
benny <vvsogi@gmail.com>
2025-08-04 17:52:18 +0900

    플레이어 이동 기능 추가 및 관련 파일 생성
    Assembly-CSharp.csproj에 Player.cs 파일을 추가하였고, Player.cs 파일에는 플레이어의 이동을 처리하는 MonoBehaviour 클래스가 구현되었습니다.
    
    이 클래스는 입력 시스템을 사용하여 이동 벡터를 읽고, 이를 기반으로 플레이어의 위치를 업데이트합니다.

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 moveVector;
+    private InputAction moveAction;
+
+    private void Start()
+    {
+        // Start
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    private void Update()
+    {
+        // Update
+        moveVector = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(moveVector.x, moveVector.y, 0f) * Time.fixedDeltaTime;
+    }
+}

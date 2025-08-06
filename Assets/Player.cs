5c7cdd59c8a796a3456654b688b20e988193726f
benny <vvsogi@gmail.com>
2025-08-07 01:40:01 +0900

    C# 코딩 규칙 추가 및 Player.cs 수정
    .editorconfig에 C# 파일에 대한 코딩 규칙과 네이밍 규칙을 추가했습니다.
    Player.cs에서 moveSpeed 중복 선언을 제거하고,
    Update 메서드의 moveVector 처리 방식을 수정했습니다.
    FixedUpdate 메서드에서 Y축 이동을 제거하고,
    calculateFacing 메서드를 추가하여 플레이어의 방향을 계산하도록 개선했습니다.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,29 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 5;
+    public Rigidbody2D rigidBody2D;
+
     private Vector2 moveVector;
     private InputAction moveAction;
-    public float moveSpeed = 5;
 
     private void Start()
     {
-        // Start
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
+
     private void Update()
     {
-        // Update
         moveVector = moveAction.ReadValue<Vector2>();
+
+        this.calculateFacing();
     }
+
 
     private void FixedUpdate()
     {
-        transform.position += new Vector3(moveVector.x, moveVector.y, 0f) * Time.fixedDeltaTime * moveSpeed;
+        transform.position += new Vector3(moveVector.x, 0, 0) * Time.fixedDeltaTime * moveSpeed;
+    }
+
+
+    private void calculateFacing()
+    {
+        if (moveVector.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+
+        if (moveVector.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }

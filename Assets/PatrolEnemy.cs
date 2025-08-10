bdfcbca1c1e1f550ff1f1c688f3464957452cf7c
benny <vvsogi@gmail.com>
2025-08-11 01:56:54 +0900

    PatrolEnemy 클래스에 공격 범위 및 추적 거리 추가, 애니메이션 상태 업데이트

--- Assets/PatrolEnemy.cs
+++ Assets/PatrolEnemy.cs
@@ -15,8 +15,15 @@ public class PatrolEnemy : MonoBehaviour
 {
     public float moveSpeed = 2f;
     public float distance = 0.6f;
+    public float attackRange = 10f;
+    public float retrieveDistance = 2.5f;
+    public float chaseSpeed = 4f;
+    public bool inRange = false;
+
     public Transform checkPoint;
+    public Transform player;
     public LayerMask layerMask;
+    public Animator animator;
 
     private Direction characterDirection = Direction.Left;
 
@@ -28,6 +35,31 @@ public class PatrolEnemy : MonoBehaviour
 
     void Update()
     {
+        if (Vector2.Distance(transform.position, player.position) <= attackRange)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+
+
+        if (inRange)
+        {
+            if (Vector2.Distance(transform.position, player.position) > retrieveDistance)
+            {
+                animator.SetBool("Attack1", false);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Attack1", true);
+            }
+
+            return;
+        }
+
         transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
 
         RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
@@ -55,6 +87,9 @@ public class PatrolEnemy : MonoBehaviour
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawRay(checkPoint.position, Vector2.down * distance);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
     }
 
 

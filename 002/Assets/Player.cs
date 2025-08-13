21eea754d268e7c473f67032d58767b8eae7677f
benny <vvsogi@gmail.com>
2025-08-14 01:33:45 +0900

    플레이어 점프 기능 추가 및 애니메이션 상태 업데이트

--- 002/Assets/Player.cs
+++ 002/Assets/Player.cs
@@ -5,12 +5,13 @@ public class Player : MonoBehaviour
 {
     public string playerName = "Benny";
     public float moveSpeed = 2.5f;
+    public float jumpForce = 6f;
 
     public Rigidbody2D rigidBody;
 
-    private float vectorX;
+    private bool isCanJump = true;
+    private float vectorX = 0;
     private InputAction inputAction;
-
 
     void Start()
     {
@@ -20,13 +21,37 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Vector2 moveVector = inputAction.ReadValue<Vector2>();
-        vectorX = normalizeVectorX(moveVector.x);
+        vectorX = NormalizeVectorX(moveVector.x);
         rigidBody.linearVelocity = new Vector2(vectorX * moveSpeed, rigidBody.linearVelocity.y);
 
-        changeFaceDirection(vectorX);
+        if (vectorX > 0)
+        {
+            ChangeFaceRight();
+        }
+
+        if (vectorX < 0)
+        {
+            ChangeFaceLeft();
+        }
+
+        if (Keyboard.current[Key.Space].isPressed && isCanJump)
+        {
+            Jump();
+        }
     }
 
-    private float normalizeVectorX(float vectorX)
+    private void Jump()
+    {
+        rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
+        changeJumpState(false);
+    }
+
+    private void changeJumpState(bool state)
+    {
+        isCanJump = state;
+    }
+
+    private float NormalizeVectorX(float vectorX)
     {
         if (vectorX > 0)
         {
@@ -40,16 +65,21 @@ public class Player : MonoBehaviour
         return 0;
     }
 
-    private void changeFaceDirection(float vectorX)
+    private void ChangeFaceLeft()
     {
-        if (vectorX > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
+        transform.eulerAngles = new Vector3(0, -180f, 0);
+    }
 
-        if (vectorX < 0)
+    private void ChangeFaceRight()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            transform.eulerAngles = new Vector3(0, -180f, 0);
+            changeJumpState(true);
         }
     }
 }

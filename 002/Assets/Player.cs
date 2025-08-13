ddfb26c3a2411c2ab41d96e470f2407bc3e485ab
benny <vvsogi@gmail.com>
2025-08-14 01:14:36 +0900

    Add player movement logic and input handling

--- 002/Assets/Player.cs
+++ 002/Assets/Player.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string playerName = "Benny";
+    public float moveSpeed = 2.5f;
+
+    public Rigidbody2D rigidBody;
+
+    private float vectorX;
+    private InputAction inputAction;
+
+
     void Start()
     {
-        
+        inputAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 moveVector = inputAction.ReadValue<Vector2>();
+        vectorX = normalizeVectorX(moveVector.x);
+        rigidBody.linearVelocity = new Vector2(vectorX * moveSpeed, rigidBody.linearVelocity.y);
+
+        changeFaceDirection(vectorX);
+    }
+
+    private float normalizeVectorX(float vectorX)
+    {
+        if (vectorX > 0)
+        {
+            return 1f;
+        }
+        else if (vectorX < 0)
+        {
+            return -1f;
+        }
+
+        return 0;
+    }
+
+    private void changeFaceDirection(float vectorX)
+    {
+        if (vectorX > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (vectorX < 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180f, 0);
+        }
     }
 }

2b50d695fb30ae5163e7cfc1ad84c6497aaf3972
benny <vvsogi@gmail.com>
2025-08-17 01:32:54 +0900

    EnemySkeleton 클래스에 방향 전환 및 체크 거리 기능 추가

--- 002/Assets/Scripts/Enemy/EnemySkeleton.cs
+++ 002/Assets/Scripts/Enemy/EnemySkeleton.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 
+public enum Direction
+{
+    Left,
+    Right
+}
+
 public class EnemySkeleton : Enemy
 {
     public float moveSpeed = 2f;
+    public float checkDistance = 0.6f;
 
-    private float vectorX = 0;
+    [SerializeField] private LayerMask checkLayerMask;
+    [SerializeField] private Transform checkPoint;
+
     private Rigidbody2D rb;
-
+    private Direction facing = Direction.Right;
 
     public void ChangeFaceLeft()
     {
@@ -26,5 +35,24 @@ public class EnemySkeleton : Enemy
     private void Update()
     {
         rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        RaycastHit2D checkWall = Physics2D.Raycast(checkPoint.position, Vector2.down, checkDistance, checkLayerMask);
+
+        if (checkWall && facing == Direction.Right)
+        {
+            ChangeFaceLeft();
+            facing = Direction.Left;
+        }
+
+        if (checkWall && facing == Direction.Left)
+        {
+            ChangeFaceRight();
+            facing = Direction.Right;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(checkPoint.position, Vector2.down * checkDistance);
     }
 }

f5e83ae3a1822fd17d7fb1f5254e3dcbb04fa65d
benny <vvsogi@gmail.com>
2025-08-17 01:43:54 +0900

    EnemySkeleton 클래스에 드롭 체크 기능 추가 및 이동 로직 개선

--- 002/Assets/Scripts/Enemy/EnemySkeleton.cs
+++ 002/Assets/Scripts/Enemy/EnemySkeleton.cs
@@ -10,9 +10,12 @@ public class EnemySkeleton : Enemy
 {
     public float moveSpeed = 2f;
     public float checkDistance = 0.6f;
+    public float dropDistance = 2f;
 
     [SerializeField] private LayerMask checkLayerMask;
     [SerializeField] private Transform checkPoint;
+    [SerializeField] private LayerMask dropLayerMask;
+    [SerializeField] private Transform dropPoint;
 
     private Rigidbody2D rb;
     private Direction facing = Direction.Right;
@@ -34,19 +37,55 @@ public class EnemySkeleton : Enemy
 
     private void Update()
     {
-        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        if (facing == Direction.Right)
+        {
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        }
+
+        if (facing == Direction.Left)
+        {
+            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+        }
+
+        handleFacing();
+        handleDropCheck();
+    }
+
+    private void handleDropCheck()
+    {
+        RaycastHit2D checkGround = Physics2D.Raycast(dropPoint.position, Vector2.down, dropDistance, dropLayerMask);
+
+        if (!checkGround && facing == Direction.Right)
+        {
+            ChangeFaceLeft();
+            facing = Direction.Left;
+            return;
+        }
+
+        if (!checkGround && facing == Direction.Left)
+        {
+            ChangeFaceRight();
+            facing = Direction.Right;
+            return;
+        }
+    }
+
+    private void handleFacing()
+    {
         RaycastHit2D checkWall = Physics2D.Raycast(checkPoint.position, Vector2.down, checkDistance, checkLayerMask);
 
         if (checkWall && facing == Direction.Right)
         {
             ChangeFaceLeft();
             facing = Direction.Left;
+            return;
         }
 
         if (checkWall && facing == Direction.Left)
         {
             ChangeFaceRight();
             facing = Direction.Right;
+            return;
         }
     }
 
@@ -54,5 +93,8 @@ public class EnemySkeleton : Enemy
     {
         Gizmos.color = Color.green;
         Gizmos.DrawRay(checkPoint.position, Vector2.down * checkDistance);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(dropPoint.position, Vector2.down * dropDistance);
     }
 }
